dcdb354791eb6493e06631024fe6efbb3de8bfa5
Pedro <pedro_rga@hotmail.com>
2019-11-08 12:30:57 -0300

    alter botão

--- Botoes/helpBotao.py
+++ Botoes/helpBotao.py
@@ -7,13 +7,15 @@ def getEstado(gpio=21):
     # led = LED(26)
 
     estado = "desligado"
-    # while True:
-    if not botao.is_active:
-        if estado == "desligado":
-            # led.on()
-            estado = "ligado"
-            return 1
-        else:
-            # led.off()
-            estado = "desligado"
-            return 0
+    return botao.is_active
+    #
+    # # while True:
+    # if not botao.is_active:
+    #     if estado == "desligado":
+    #         # led.on()
+    #         estado = "ligado"
+    #         return 1
+    #     else:
+    #         # led.off()
+    #         estado = "desligado"
+    #         return 0
